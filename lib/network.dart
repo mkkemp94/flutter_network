81412e031039bf131cced1f6676447683222184d
MKemp <mkemp@accu-time.com>
2023-10-10 17:07:00 -0400

    Add authenticated header

--- lib/network.dart
+++ lib/network.dart
@@ -1,11 +1,16 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
 Future<Album> fetchAlbum() async {
   // get() returns a [Future] that contains a [Response]
-  final http.Response response = await http
-      .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+  final http.Response response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/albums/1'),
+      headers: {
+        HttpHeaders.authorizationHeader: 'Basic your_api_token_here',
+      },
+  );
 
   if (response.statusCode == 200) {
     // OK
