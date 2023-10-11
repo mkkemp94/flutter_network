7f5f896c42587e66333821c256fb8b96ac652edb
MKemp <mkemp@accu-time.com>
2023-10-11 16:18:12 -0400

    Add update network data capability to project

--- lib/main.dart
+++ lib/main.dart
@@ -31,15 +31,14 @@ class NetworkScreen extends StatefulWidget {
 }
 
 class _NetworkScreenState extends State<NetworkScreen> {
-  late Future<Album> _fetchedAlbum; // To get
-  Future<Album>? _createdAlbum; // To create
   final TextEditingController _controller = TextEditingController();
+  late Future<Album> _futureAlbum;
 
   @override
   void initState() {
     // Start of app
     super.initState();
-    _fetchedAlbum = fetchAlbum(); // Get album
+    _futureAlbum = fetchAlbum(); // Get album
   }
 
   @override
@@ -49,118 +48,71 @@ class _NetworkScreenState extends State<NetworkScreen> {
 
     return Scaffold(
       // Body
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // C1
-            Text(
-              'Fetched Album:',
-              style: style,
-            ),
-
-            // C2
-            buildFetchedAlbumSection(style),
-
-            // C3
-            const SizedBox(
-              height: 100,
-            ),
-
-            // C4
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(18),
-              child: (_createdAlbum == null)
-                  ? buildCreateAlbumSection(style)
-                  : buildShowCreatedAlbumSection(style),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  FutureBuilder<Album> buildFetchedAlbumSection(TextStyle style) {
-    return FutureBuilder<Album>(
-      future: _fetchedAlbum,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Text(
-            // Success
-            snapshot.data!.title,
-            style: style,
-          );
-        } else if (snapshot.hasError) {
-          return Text(
-            // Error
-            '${snapshot.error}',
-            style: style,
-          );
-        }
-        // Loading
-        return const CircularProgressIndicator();
-      },
-    );
-  }
-
-  Column buildCreateAlbumSection(TextStyle style) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        // C1
-        TextField(
-          controller: _controller,
-          decoration:
-              // Edit text
-              InputDecoration(hintText: 'Enter Title', hintStyle: style),
-        ),
-
-        // C2
-        ElevatedButton(
-            onPressed: () {
-              // On click
-              setState(() {
-                _createdAlbum = createAlbum(_controller.text);
-              });
-            },
-            child: Text(
-              'Create Album',
-              style: style,
-            )),
-      ],
-    );
-  }
-
-  Column buildShowCreatedAlbumSection(TextStyle style) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        // C1
-        FutureBuilder<Album>(
-            future: _createdAlbum,
+      body: Container(
+        alignment: Alignment.center,
+        // Future
+        child: FutureBuilder<Album>(
+            future: _futureAlbum,
             builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                // Success
-                return Text(style: style, snapshot.data!.title);
-              } else if (snapshot.hasError) {
-                // Error
-                return Text(style: style, '${snapshot.error}');
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  // Success
+                  // Column
+                  return Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        // C1
+                        Text(
+                          'Fetched Album:',
+                          style: style,
+                        ),
+
+                        // C2
+                        Text(
+                          snapshot.data!.title,
+                          style: style,
+                        ),
+
+                        // C3
+                        const SizedBox(
+                          height: 100,
+                        ),
+
+                        // C4
+                        TextField(
+                          controller: _controller,
+                          decoration:
+                              // Edit text
+                              InputDecoration(
+                                  hintText: 'Enter Title', hintStyle: style),
+                        ),
+
+                        // C5
+                        ElevatedButton(
+                          onPressed: () {
+                            // On click
+                            setState(() {
+                              _futureAlbum = updateAlbum(_controller.text);
+                            });
+                          },
+                          child: Text(
+                            'Update Album',
+                            style: style,
+                          ),
+                        ),
+                      ]);
+                } else if (snapshot.hasError) {
+                  // Error
+                  return Text(
+                    '${snapshot.error}',
+                    style: style,
+                  );
+                }
               }
               // Loading
               return const CircularProgressIndicator();
             }),
-
-        // C2
-        ElevatedButton(
-            onPressed: () {
-              // On click
-              setState(() {
-                _createdAlbum = null;
-              });
-            },
-            child: Text(style: style, 'Reset Album')),
-      ],
+      ),
     );
   }
 }
